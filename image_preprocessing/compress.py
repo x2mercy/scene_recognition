19e0400eaf956f7446aae383e48fb03e37623282
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-10-26 13:43:54 -0400

    Update compress.py

--- image_preprocessing/compress.py
+++ image_preprocessing/compress.py
@@ -10,30 +10,25 @@ Created on Mon Oct 23 19:38:00 2017
 from PIL import Image  
 import os
 
-#图片压缩批处理  
+
 def compressImage(srcPath,dstPath):  
     for filename in os.listdir(srcPath):  
-        #如果不存在目的目录则创建一个，保持层级结构
         if not os.path.exists(dstPath):
                 os.makedirs(dstPath)        
 
-        #拼接完整的文件或文件夹路径
         srcFile=os.path.join(srcPath,filename)
         dstFile=os.path.join(dstPath,filename)
         print srcFile
         print dstFile
 
-        #如果是文件就处理
         if os.path.isfile(srcFile):     
-            #打开原图片缩小后保存，可以用if srcFile.endswith(".jpg")或者split，splitext等函数等针对特定文件压缩
             sImg=Image.open(srcFile)  
             w,h=sImg.size  
             print w,h
-            dImg=sImg.resize((w/5,h/5),Image.ANTIALIAS)  #设置压缩尺寸和选项，注意尺寸要用括号
-            dImg.save(dstFile) #也可以用srcFile原路径保存,或者更改后缀保存，save这个函数后面可以加压缩编码选项JPEG之类的
+            dImg=sImg.resize((w/5,h/5),Image.ANTIALIAS) 
+            dImg.save(dstFile) 
             print dstFile+" compressed succeeded"
 
-        #如果是文件夹就递归
         if os.path.isdir(srcFile):
             compressImage(srcFile,dstFile)
 
